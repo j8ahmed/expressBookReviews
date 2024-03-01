a989448ff19db21a280ae99a24fe1bdc75f191b6
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-01 17:02:39 -0500

    add basic routes for books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.status(200).json(books)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+    const { isbn } = req.params
+    const booksFiltered = Object.keys(books).map(key => books[key]).filter((book) => {
+        return book.isbn === isbn
+    })
+    res.send(booksFiltered)
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { author } = req.params
+    const booksFiltered = Object.keys(books).map(key => books[key]).filter((book) => {
+        return book.author.includes(author)
+    })
+    res.send(booksFiltered)
 });
 
 // Get all books based on title

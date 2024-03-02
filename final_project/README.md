5d702befb9e54ef1e32d4b24c2779b8f6326dfdc
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-02 04:56:26 -0500

    remove console.log used in testing

--- final_project/README.md
+++ final_project/README.md
@@ -1,1 +1,5 @@
 Practice-Project
+
+
+- [Original repo - GitHub](https://github.com/ibm-developer-skills-network/expressBookReviews)
+- [My forked version of the repo - GitHub](https://github.com/j8ahmed/expressBookReviews)
--- final_project/index.js
+++ final_project/index.js
@@ -14,7 +14,6 @@ app.use("/customer/auth/*", function auth(req, res, next){
     //Write the authenication mechanism here
     if(req.session.authorization) {
         const token = req.session.authorization['accessToken']; // Access Token
-        console.log(token)
         jwt.verify(token, "access", (err, user) => {
             if (err) {
                 return res.status(403).json({message: "User not authenticated"})

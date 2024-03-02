9d2ebe814cb20e083da04e289262f3f230e29e8a
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-02 04:48:48 -0500

    make isbn/:isbn route async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,6 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
-
     const myPromise = new Promise((resolve,reject) => {
         setTimeout(() => {
             resolve(books)
@@ -32,14 +31,15 @@ public_users.get('/', async function (req, res) {
     }).then(data => {
         return res.status(200).json(data)
     })
+});
 
 /* Code to call enpoint from site and return the readable stream
 
 (function testGetAsync(){
     fetch("http://localhost:5000/")
-        .then(res => {
-            console.log(res)
-            console.log(res.body)
+        .then(async res => {
+            const data = await res.json()
+            console.log(data)
         })
         .catch(err => {
             console.error(err)
@@ -48,16 +48,37 @@ public_users.get('/', async function (req, res) {
 
 */
 
-});
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
     const { isbn } = req.params
     const booksFiltered = Object.keys(books).map(key => books[key]).filter((book) => {
         return book.isbn === isbn
     })
-    res.send(booksFiltered)
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(books)
+        },6000)
+    }).then(data => {
+        return res.status(200).json(booksFiltered)
+    })
 });
+
+/* Code to call enpoint from site and return the readable stream
+
+(function testGetAsync(){
+    fetch("http://localhost:5000/isbn/9780435272463")
+        .then(async res => {
+            const data = await res.json()
+            console.log(data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})()
+
+*/
+
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {

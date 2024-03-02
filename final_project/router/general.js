7e153cb02881959341063a3f6053f0da0c0f09c5
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-02 04:40:29 -0500

    update routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,13 +6,48 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body
+    if (!username)
+        return res.status(400).send("No username provided")
+    else if (!password)
+        return res.status(400).send("No password provided")
+
+    const usersFiltered = users.filter(user => user.username === username)
+    if (usersFiltered.length > 0)
+        return res.status(400).send("username already exists")
+
+    users.push({username, password})
+    console.log(users)
+
+    res.status(200).send(`User: ${username} has been added successfully.`)
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.status(200).json(books)
+public_users.get('/', async function (req, res) {
+
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(books)
+        },6000)
+    }).then(data => {
+        return res.status(200).json(data)
+    })
+
+/* Code to call enpoint from site and return the readable stream
+
+(function testGetAsync(){
+    fetch("http://localhost:5000/")
+        .then(res => {
+            console.log(res)
+            console.log(res.body)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})()
+
+*/
+
 });
 
 // Get book details based on ISBN
@@ -35,14 +70,21 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { title } = req.params
+    const booksFiltered = Object.keys(books).map(key => books[key]).filter((book) => {
+        return book.title.includes(title)
+    })
+    res.send(booksFiltered)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { isbn } = req.params
+    const booksFiltered = Object.keys(books).map(key => books[key]).filter((book) => {
+        return book.isbn === isbn
+    })
+    const reviews = booksFiltered.map(book => ({title: book.title, isbn: book.isbn, reviews: book.reviews}))
+    res.send(reviews)
 });
 
 module.exports.general = public_users;
